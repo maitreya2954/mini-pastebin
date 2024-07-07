d9c81c337b0b8e950084a37bec8792c02f1ad76c
Siddharth Rayabharam <maitreya2954@gmail.com>
2024-07-07 18:50:48 -0400

    Server file moved

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use('/api', snippetRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9999;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
