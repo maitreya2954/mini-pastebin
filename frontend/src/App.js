06b8c6b96f8698679853e96b522cf7879c0a4c68
Siddharth Rayabharam <maitreya2954@gmail.com>
2024-07-07 19:28:57 -0400

    import path fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
-import SnippetDetail from './components/SnippetDetail';
+import SnippetDetail from './components/SnippetDetail/SnippetDetail';
 import CreateSnippet from './components/CreateSnippet/CreateSnippet';
 import Navbar from './components/Navbar/Navbar';
 import './App.css';
