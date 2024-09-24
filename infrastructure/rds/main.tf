c328633f6328e1e66876dc5fcf752c6fa4b36f25
Siddharth Rayabharam <maitreya2954@gmail.com>
2024-09-23 20:32:47 -0400

    skip final snapshot for aws rds

--- infrastructure/rds/main.tf
+++ infrastructure/rds/main.tf
@@ -7,6 +7,7 @@ resource "aws_db_instance" "postgres" {
   password = "mypassword"
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
   db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
+  skip_final_snapshot = true
 }
 
 resource "aws_db_subnet_group" "rds_subnet_group" {
